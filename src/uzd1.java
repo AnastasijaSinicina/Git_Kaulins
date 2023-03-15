93db8b4f3ceaef5d204fc4d2d8fdd3c707912de8
Anastasija <sinica.nastja@gmail.com>
2023-03-15 13:56:26 +0200

    Papildinats ar funkcionalitati ka metamo kaulinu var mest vairakas reizes

--- src/uzd1.java
+++ src/uzd1.java
@@ -1,10 +1,15 @@
 import java.util.Random;
 
 public class uzd1 {
-public static void main(String[]args){
+static void mestKaulinu(int reizes){
 	int sk;
 	Random rand = new Random();
+	for(int i=1; i<=reizes; i++){
 	sk = rand.nextInt(6)+1;
 	System.out.println("Uzkrita skaitlis "+sk);
+	}
+}
+public static void main(String[]args){
+	mestKaulinu(3);
 }
 }
