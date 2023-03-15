dec14dc36d7b029dc634adf7c3e3ce323b2bb037
Anastasija <sinica.nastja@gmail.com>
2023-03-15 14:00:57 +0200

    Iespejams ievadit metienu skaitu no konsoles

--- src/uzd1.java
+++ src/uzd1.java
@@ -1,4 +1,18 @@
+import java.util.Random;
+import java.util.Scanner;
 
 public class uzd1 {
+	public static void main(String[]args){
+		int sk, reiz;
+		Random rand = new Random();
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Cik reizes mest kaulinu? ");
+		reiz = scan.nextInt();
+		for(int i=1; i<=reiz; i++){
+			sk = rand.nextInt(6)+1;
+			System.out.println("Uzkrita skaitlis "+sk);
+		}
+		scan.close();
+	}
 
 }
