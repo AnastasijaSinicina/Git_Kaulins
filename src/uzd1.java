69e710cf3e064f79a530978a57157d325b1807ae
Anastasija <sinica.nastja@gmail.com>
2023-03-15 13:54:05 +0200

    Refaktorets kods un ieviesta metode

---
+++ src/uzd1.java
@@ -0,0 +1,10 @@
+import java.util.Random;
+
+public class uzd1 {
+public static void main(String[]args){
+	int sk;
+	Random rand = new Random();
+	sk = rand.nextInt(6)+1;
+	System.out.println("Uzkrita skaitlis "+sk);
+}
+}
