d57f99288376cd713421547de8255898d1e9a458
Anastasija <sinica.nastja@gmail.com>
2023-04-05 09:56:39 +0300

    Papildinats ar lielako skaitla meklesanu

--- src/Kaulins.java
+++ src/Kaulins.java
@@ -5,17 +5,18 @@ import javax.swing.JOptionPane;
 
 public class Kaulins {
 	static void mestKaulinu(int reizes) {
-		int skaitlis;
+		int skaitlis, lielak=0;
 		Random rand = new Random();
 		for(int i=1; i<=reizes; i++) {
 			skaitlis = rand.nextInt(6)+1;	
 			System.out.println("Uzkrita skaitlis "+skaitlis);
+			if(lielak<skaitlis) {
+				lielak = skaitlis;
+			}			
 		}
+		System.out.println("Lielakais skaitlis "+lielak);
 	}
-	
-	
-	
-	public static void main(String[] args) {
+		public static void main(String[] args) {
 		int reizes;
 		Scanner scan = new Scanner(System.in);
 		do {
