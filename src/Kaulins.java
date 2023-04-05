2dd1fa7848f58ef5a644ff410a133584856e77b4
Anastasija <sinica.nastja@gmail.com>
2023-04-05 09:10:21 +0300

    Noversta iespeja ievadit negativo skaitli

---
+++ src/Kaulins.java
@@ -0,0 +1,28 @@
+import java.util.Random;
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
+public class Kaulins {
+	static void mestKaulinu(int reizes) {
+		int skaitlis;
+		Random rand = new Random();
+		for(int i=1; i<=reizes; i++) {
+			skaitlis = rand.nextInt(6)+1;	
+			System.out.println("Uzkrita skaitlis "+skaitlis);
+		}
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		int reizes;
+		Scanner scan = new Scanner(System.in);
+		do {
+		System.out.println("Cik reizes mest kauliòu?");
+		reizes = scan.nextInt();
+		}while(reizes<1);
+		mestKaulinu(reizes);
+		scan.close();
+	}
+}
